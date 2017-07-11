fe8b0ce30f07c34d350bd95d58966095d0d8976d
Сухарик <suhr@i2pmail.org>
2017-07-11 14:31:01 +0300

    Rewrite it in rust^Wgfx-rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-extern crate sfml;
+#![allow(dead_code)]
+
+#[macro_use] extern crate gfx;
+extern crate gfx_device_gl;
+extern crate gfx_window_glutin;
+extern crate glutin;
+extern crate cgmath;
+extern crate palette;
+
 extern crate portmidi;
 
-use sfml::window::*;
-use sfml::graphics::*;
 use portmidi::{PortMidi, MidiMessage, OutputPort, Result as PmResult};
 
+use gfx::handle::{RenderTargetView, DepthStencilView};
+use gfx::traits::{Factory, FactoryExt};
+use gfx::{Device, Encoder, PipelineState};
+use gfx_device_gl as gl;
+use gfx_window_glutin as gfx_glutin;
+
+use cgmath::Vector2;
 
 mod ui;
 
-pub static FONT: &'static [u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/PTM55F.ttf"));
+pub type ColorFormat = gfx::format::Rgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
 
 struct MusicBox {
     port: OutputPort,
     notes: Vec<u8>,
-    sustain: bool,
 }
 
 impl MusicBox {
@@ -21,7 +34,6 @@ impl MusicBox {
         MusicBox {
             port: port,
             notes: vec![],
-            sustain: false,
         }
     }
 
@@ -56,69 +68,263 @@ impl MusicBox {
     }
 }
 
-fn proceed() {
-    let midi = PortMidi::new().unwrap();
-    let mut the_box = MusicBox::new(midi.default_output_port(1024).unwrap());
-    let view = (1920.0, 1080.0);//(960.0, 600.0);
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 4] = "a_Color",
+    }
 
-    let mut window = RenderWindow::new(
-        VideoMode::new_init(view.0 as u32, view.1 as u32, 32),
-        "Tricesimoprimal Keyboard",
-        WindowStyle::default(),
-        &ContextSettings::default().antialiasing(8),
-    ).expect("Cannot create a new Render Window.");
-    window.set_key_repeat_enabled(false);
-
-    let mut hexes = ui::Hexes::new(view.0, view.1);
-
-    loop {
-        loop {
-            use sfml::window::MouseButton::*;
-            use sfml::window::Key;
-
-            let event = window.poll_event();
-            match event {
-                Some(Event::Closed) => return,
-                Some(Event::Resized {width: w, height: h}) => {
-                    window.set_view(&View::from_rect(&Rect::new(0.0, 0.0, w as f32, h as f32)));
-                    hexes.resize(w as f32, h as f32);
-                },
-                Some(Event::MouseButtonPressed {button: Left, x, y}) => {
-                    let note = hexes.press(x, y) + 64;
-                    if note < 128 && note > 0 {
-                        drop(the_box.note_on(note as u8));
-                    }
-                },
-                Some(Event::MouseButtonReleased {button: Left, ..}) => {
-                    if !the_box.sustain {
-                        hexes.release();
-                        the_box.all_notes_off();
-                    }
-                },
-                Some(Event::KeyPressed {code: Key::Space, ..}) => {
-                    the_box.sustain = true;
-                },
-                /*Some(Event::KeyPressed {code, ctrl, ..}) => the_box.press(code, ctrl),
-                Some(Event::TextEntered {code}) => the_box.text(code),*/
-                Some(Event::KeyReleased {code: Key::Space, ..}) => {
-                    the_box.sustain = false;
-                    the_box.all_notes_off();
-                    hexes.release();
-                },
-                None => break,
-                _ => (),
-            }
-        }
-
-        window.clear(&Color::new_rgb(0x21, 0x21, 0x21));
-        window.draw(&hexes);
-
-        window.display();
-        ::std::thread::sleep(::std::time::Duration::from_millis(25));
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
     }
 }
 
+pub trait Render {
+    fn render_fan<V>(&mut self, iter: V)
+    where V: std::iter::IntoIterator<Item=Vertex>;
+}
+
+struct Renderer {
+    factory: gl::Factory,
+    encoder: Encoder<gl::Resources, gl::CommandBuffer>,
+    out_color: RenderTargetView<gl::Resources, ColorFormat>,
+    pso: PipelineState<gl::Resources, pipe::Meta>,
+    vertices: Vec<Vertex>,
+    indices: Vec<u16>,
+}
+
+impl Renderer {
+    pub fn new(
+        mut factory: gl::Factory,
+        encoder: Encoder<gl::Resources, gl::CommandBuffer>,
+        out_color: RenderTargetView<gl::Resources, ColorFormat>
+    ) -> Self {
+        use gfx::state::{Rasterizer, MultiSample};
+
+        let vs = factory.create_shader_vertex(
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/plain_150.glslv"))
+        ).unwrap();
+        let ps = factory.create_shader_pixel(
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/plain_150.glslf"))
+        ).unwrap();
+
+        let pso = factory.create_pipeline_state(
+            &gfx::ShaderSet::Simple(vs, ps),
+            gfx::Primitive::TriangleList,
+            Rasterizer {
+                samples: Some(MultiSample),
+                ..Rasterizer::new_fill()
+            },
+            pipe::new(),
+        ).expect("Failed to create a PSO");
+
+        Renderer {
+            factory, encoder, pso, out_color,
+            vertices: vec![],
+            indices: vec![],
+        }
+    }
+
+    pub fn update_views(&mut self, window: &glutin::Window, depth: &mut DepthStencilView<gl::Resources, DepthFormat>) {
+        gfx_glutin::update_views(&window, &mut self.out_color, depth)
+    }
+
+    pub fn draw(&mut self, device: &mut gl::Device) {
+        let (vbuf, sl) =
+            self.factory.create_vertex_buffer_with_slice(&self.vertices, &*self.indices);
+        let data = pipe::Data {
+            vbuf,
+            out: self.out_color.clone(),
+        };
+
+        self.encoder.clear(&data.out, BLACK);
+        self.encoder.draw(&sl, &self.pso, &data);
+        self.encoder.flush(device);
+
+        self.vertices.clear();
+        self.indices.clear();
+    }
+}
+
+impl Render for Renderer {
+    fn render_fan<V>(&mut self, iter: V)
+    where V: std::iter::IntoIterator<Item=Vertex> {
+        let i0 = self.vertices.len() as u16;
+        let mut vs = iter.into_iter();
+        self.vertices.push(vs.next().unwrap());
+        self.vertices.push(vs.next().unwrap());
+        for (i, v) in vs.enumerate() {
+            let i = i as u16 + 1;
+            self.vertices.push(v);
+            self.indices.extend(&[i0, i0+i, i0+i+1]);
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Msg {
+    Resized(Vector2<f32>),
+    LeftPressed(Vector2<f32>),
+    LeftReleased,
+    Keyboard(glutin::ElementState, glutin::VirtualKeyCode)
+}
+
+#[derive(Debug)]
+struct Intent {
+    mouse_pos: Vector2<f32>
+}
+
+impl Intent {
+    fn new() -> Self {
+        Intent {
+            mouse_pos: Vector2::new(0.0, 0.0)
+        }
+    }
+
+    fn intent(&mut self, ev: glutin::WindowEvent) -> Option<Msg> {
+        use glutin::WindowEvent as E;
+        use glutin::ElementState::*;
+        use glutin::MouseButton::{Left};
+
+        Some(match ev {
+            E::Resized(w, h) => Msg::Resized(Vector2::new(w as f32, h as f32)),
+            E::MouseMoved(w, h) => {
+                self.mouse_pos = Vector2::new(w as f32, h as f32);
+                return None
+            },
+            E::MouseInput(Pressed, Left) => Msg::LeftPressed(self.mouse_pos),
+            E::MouseInput(Released, Left) => Msg::LeftReleased,
+            E::KeyboardInput(es, _, Some(vk), _) => Msg::Keyboard(es, vk),
+            _ => return None,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Model {
+    hexes: ui::Hexes,
+    notes: Vec<u8>,
+    sustain: bool,
+}
+
+impl Model {
+    fn new(size: Vector2<f32>) -> Self {
+        Model {
+            hexes: ui::Hexes::new(size, vec![]),
+            notes: vec![],
+            sustain: false,
+        }
+    }
+}
+
+fn model(model: Model, msg: Msg) -> Model {
+    use Msg::*;
+    use glutin::ElementState::*;
+    use glutin::VirtualKeyCode::*;
+
+    let Model { mut hexes, mut notes, mut sustain } = model;
+
+    match msg {
+        Resized(wh) =>
+            hexes.size = wh,
+        LeftPressed(xy) => {
+            let note = hexes.press(xy);
+            notes.push(note)
+        },
+        LeftReleased if !sustain => {
+            hexes.release_all();
+            notes.clear()
+        },
+        Keyboard(Pressed, Space) =>
+            sustain = true,
+        Keyboard(Released, Space) => {
+            sustain = false;
+            hexes.release_all();
+            notes.clear()
+        },
+        _ => (),
+    };
+
+    Model { hexes, notes, sustain }
+}
+
+fn draw(model: &Model, renderer: &mut Renderer) {
+    model.hexes.draw(renderer)
+}
+
+fn update_midi(model: &Model, the_box: &mut MusicBox) {
+    let mn = model.notes.len();
+    let bn = the_box.notes.len();
+    if mn > bn {
+        for n in &model.notes[bn..] {
+            drop(the_box.note_on(*n))
+        }
+    }
+    else if mn == 0 {
+        the_box.all_notes_off()
+    }
+}
+
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
 fn main() {
-    proceed()
+    let events_loop = glutin::EventsLoop::new();
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Tricesimoprimal Keyboard".to_string())
+        .with_dimensions(960, 600)
+        .with_multisampling(8)
+        .with_vsync();
+    let (window, mut device, mut factory, main_color, mut main_depth) =
+        gfx_glutin::init::<ColorFormat, DepthFormat>(builder, &events_loop);
+
+    let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let mut renderer = Renderer::new(factory, encoder, main_color);
+
+    let midi = PortMidi::new().unwrap();
+    let port = midi.default_output_port(1024).unwrap();
+    let mut the_box = MusicBox::new(port);
+
+    let mut mailbox = vec![];
+    let mut intent = Intent::new();
+    let mut the_model = Model::new(Vector2::new(960.0, 600.0));
+
+    let mut running = true;
+    let mut needs_update = true;
+    while running {
+        events_loop.poll_events(|glutin::Event::WindowEvent{window_id: _, event}| {
+            use glutin::WindowEvent::*;
+            match event {
+                Closed => running = false,
+                Resized(w, h) => {
+                    renderer.update_views(&window, &mut main_depth);
+                    if let Some(msg) = intent.intent(Resized(w, h)) {
+                        mailbox.push(msg);
+                    }
+                },
+                ev =>
+                    if let Some(msg) = intent.intent(ev) {
+                        mailbox.push(msg);
+                    },
+            }
+            needs_update = true
+        });
+
+        if needs_update {
+            for m in mailbox.drain(0..) {
+                the_model = model(the_model, m)
+            }
+
+            draw(&the_model, &mut renderer);
+            update_midi(&the_model, &mut the_box);
+            renderer.draw(&mut device);
+            window.swap_buffers().unwrap();
+            device.cleanup();
+
+            needs_update = false;
+        }
+
+        let dt = ::std::time::Duration::from_millis(10);
+        ::std::thread::sleep(dt);
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,53 +1,112 @@
-use sfml::graphics::{Drawable, RenderTarget, RenderStates, CircleShape, Color, Shape, Transformable};
-use sfml::system::Vector2f;
+use cgmath::prelude::*;
+use cgmath::{Vector2, Vector3, Matrix2, Rad, Deg, Rotation2, Basis2};
 
-/*
-const GAP: f32 = 0.175;
+macro_rules! rgb {
+    ($r:expr, $g:expr, $b:expr) => {
+        Vector3 {
+            x: $r as f32 / 255.0,
+            y: $g as f32 / 255.0,
+            z: $b as f32 / 255.0
+        }
+    }
+}
 
-const HORIZ_X: f32 = 0.866 * (2.0 + GAP);
-const DIAG_X: f32 = 0.5 * HORIZ_X;
-const DIAG_Y: f32 = 1.5 + 0.707 * GAP;
-*/
-
-static COLORS: [(u8, u8, u8); 5] = [
-    (0xff, 0xff, 0xff),
-    (0xcf, 0xcf, 0xcf),
-    (0xbb, 0xaa, 0x93),
-    (0x7b, 0x7b, 0x7b),
-    (0xff, 0x9f, 0x41),
+static COLORS: [Vector3<f32>; 5] = [
+    rgb!(0xff, 0xff, 0xff),
+    rgb!(0xcf, 0xcf, 0xcf),
+    rgb!(0xbb, 0xaa, 0x93),
+    rgb!(0x7b, 0x7b, 0x7b),
+    rgb!(0xff, 0x9f, 0x41),
 ];
 
-type Coord = (i32, i32);
+type Xy = Vector2<f32>;
+type Qr<T> = Vector2<T>;
+type Color = Vector3<f32>;
 
+fn into_lrgb(rgb: Vector3<f32>) -> [f32; 4] {
+    use palette::pixel::Srgb;
+    let rgb: ::palette::Rgb = Srgb::new(rgb.x, rgb.y, rgb.z).into();
+    rgb.to_pixel()
+}
+
+// Combining http://www.redblobgames.com/grids/hexagons/#hex-to-pixel with rotation
+
+fn into_xy(qr: Qr<f32>, radius: f32, angle: Rad<f32>) -> Xy {
+    let rot: Basis2<f32> = Rotation2::from_angle(angle);
+
+    let mat = Matrix2::new(
+        1.732, 0.0,
+        0.866, -1.5,
+
+    );
+
+    rot.rotate_vector(mat * qr * radius)
+}
+
+
+fn into_qr(xy: Xy, radius: f32, angle: Rad<f32>) -> Qr<f32> {
+    let rot: Basis2<f32> = Rotation2::from_angle(-angle);
+
+    let mat = Matrix2::new(
+        0.577, 0.0,
+        0.333, -0.667,
+    );
+
+    mat * rot.rotate_vector(xy) / radius
+}
+
+fn round_qr(qr: Qr<f32>) -> Qr<i32> {
+    let (q, r, s) = (qr.x, qr.y, -qr.x - qr.y);
+    let (rq, rr, rs) = (q.round(), r.round(), s.round());
+    let (dq, dr, ds) = (rq - q, rr - r, rs - s);
+
+    if dq > dr && dq > ds {
+        Vector2::new((-rr - rs) as i32, rr as i32)
+    }
+    else if dr > ds {
+        Vector2::new(rq as i32, (-rq - rs) as i32)
+    }
+    else {
+        Vector2::new(rq as i32, rr as i32)
+    }
+}
+
+fn hex_corner(center: Xy, size: f32, angle: Rad<f32>, i: u8) -> Xy {
+    let phi = ::std::f32::consts::PI / 3.0;
+    let angle = angle + Rad(i as f32 * phi);
+    let rot: Basis2<f32> = Rotation2::from_angle(angle);
+    center + rot.rotate_vector(Vector2 { x: 0.0, y: size })
+}
+
+#[derive(Debug)]
 pub struct Hexes {
-    radius: f32,
-    width: f32,
-    height: f32,
-    angle: f32,
-    pressed: Vec<Coord>,
+    pub size: Vector2<f32>,
+    pub hex_size: f32,
+    pub hex_gap: f32,
+    pub angle: Rad<f32>,
+    pub pressed: Vec<Qr<i32>>,
+}
+
+fn midi_note(qr: Qr<i32>) -> u8 {
+    (5*qr.x + 3*qr.y + 64) as u8
 }
 
 impl Hexes {
-    pub fn new(width: f32, height: f32) -> Hexes {
+    pub fn new(size: Vector2<f32>, pressed: Vec<Qr<i32>>) -> Self {
         Hexes {
-            radius: 40.0,
-            width: width,
-            height: height,
-            angle: -16.102113752,
-            pressed: vec![],
+            size,
+            hex_size: 80.0,
+            hex_gap: 2.0,
+            angle: Deg(16.102113752).into(),
+            pressed,
         }
     }
 
-    pub fn resize(&mut self, width: f32, height: f32) {
-        self.width = width;
-        self.height = height;
-    }
-
-    fn hex_color(&self, c: Coord) -> Color {
-        let n = match 5 * c.0 + 3 * c.1 {
-            p if p >= 0 => p % 31,
-            n => (248 + n) % 31,
-        };
+    fn hex_color(&self, c: Qr<i32>) -> Color {
+        let n = match 5*c.x + 3*c.y {
+            p if p >= 0 => p,
+            n => 248 + n,
+        } % 31;
         let cn = match n {
             0 | 5 | 10 | 13 | 18 | 23 | 28 => 0,
             2 | 7 | 12 | 15 | 20 | 25 | 30 => 1,
@@ -58,149 +117,54 @@ impl Hexes {
 
         let rgb = COLORS[cn];
         if self.pressed.contains(&c) {
-            Color::new_rgb(rgb.0, rgb.1, rgb.2) * Color::new_rgb(0xb0, 0xc0, 0xd0)
+            0.5 * rgb
         } else {
-            Color::new_rgb(rgb.0, rgb.1, rgb.2)
+            rgb
         }
     }
 
-    pub fn press(&mut self, x: i32, y: i32) -> i32 {
-        let center = Vector2f {
-            x: self.width / 2.0,
-            y: self.height / 2.0
-        };
-        let xy = Vector2f { x: x as f32, y: y as f32 } - center;
-        let qr = into_qr(xy, self.radius, self.angle);
-        self.pressed.push(qr);
+    pub fn press(&mut self, xy: Xy) -> u8 {
+        let xy = 2.0 * xy - self.size;
+        let xy = Vector2::new(xy.x, -xy.y);
 
-        5 * qr.0 + 3 * qr.1
+        let qr = round_qr(into_qr(xy, self.hex_size + self.hex_gap, self.angle));
+        self.pressed.push(qr);
+        midi_note(qr)
     }
 
-    pub fn release(&mut self) {
+    pub fn release_all(&mut self) {
         self.pressed.clear()
     }
-
-    pub fn hl_hex(&mut self, x: i32, y: i32) {
-        let center = Vector2f {
-            x: self.width / 2.0,
-            y: self.height / 2.0
-        };
-        let xy = Vector2f { x: x as f32, y: y as f32 } - center;
-        let qr = into_qr(xy, self.radius, self.angle);
-        self.pressed = vec![qr];
-    }
 }
 
-// Combining http://www.redblobgames.com/grids/hexagons/#hex-to-pixel with rotation
-fn into_xy(qr: Coord, radius: f32, angle: f32) -> Vector2f {
-    let (q, r) = (qr.0 as f32, qr.1 as f32);
-    let angle = 3.14159 * (angle / 180.0);
-    let (cos, sin) = (angle.cos(), angle.sin());
+impl Hexes {
+    pub fn draw<R: super::Render>(&self, renderer: &mut R) {
+        let width = self.size.x / self.size.y;
+        let size = self.hex_size / self.size.y;
+        let gap = self.hex_gap / self.size.y;
 
-    let (a11, a12) = (1.732*cos, 0.866*cos - 1.5*sin);
-    let (a21, a22) = (1.732*sin, 0.866*sin + 1.5*cos);
+        let c0 = round_qr(into_qr(Vector2::new(-width, -1.0), size, -self.angle));
+        let c1 = round_qr(into_qr(Vector2::new(width, 1.0), size, -self.angle));
 
-    let (x, y) = (a11*q + a12*r, a21*q + a22*r);
-
-    Vector2f { x: radius * x, y: radius * y }
-}
-
-fn into_qr(xy: Vector2f, radius: f32, angle: f32) -> (i32, i32) {
-    let (x, y) = (xy.x, xy.y);
-    let angle = 3.14159 * (angle / 180.0);
-    let (cos, sin) = (angle.cos(), angle.sin());
-    let (a11, a12) = (0.577 * cos + 0.333 * sin, 0.577 * sin - 0.333 * cos);
-    let (a21, a22) = (-0.667 * sin, 0.667 * cos);
-
-    let (q, r) = ((a11*x + a12*y) / radius, (a21*x + a22*y) / radius);
-    let s = -q - r;
-
-    let (rq, rr, rs) = (q.round(), r.round(), s.round());
-    let (dq, dr, ds) = (rq - q, rr - r, rs - s);
-    if dq > dr && dq > ds {
-        ((-rr - rs) as i32, rr as i32)
-    }
-    else if dr > ds {
-        (rq as i32, (-rq - rs) as i32)
-    }
-    else {
-        (rq as i32, rr as i32)
-    }
-}
-
-fn is_outside(hx: &Hexes, xy: Vector2f) -> bool {
-    let pos = (
-        xy.x + hx.width / 2.0,
-        xy.y + hx.height / 2.0
-    );
-
-    pos.0 < 0.0 || pos.0 > hx.width ||
-    pos.1 < 0.0 || pos.1 > hx.height
-}
-
-
-impl Drawable for Hexes {
-    fn draw(&self, target: &mut RenderTarget, rs: &mut RenderStates) {
-        let center = Vector2f {
-            x: self.width / 2.0,
-            y: self.height / 2.0
-        };
-        let mut cs = CircleShape::new_init(self.radius, 6);
-        cs.set_origin(&Vector2f {
-            x: self.radius,
-            y: self.radius,
-        });
-        cs.set_fill_color(&self.hex_color((0,0)));
-        cs.set_outline_thickness(2.);
-        cs.set_outline_color(&Color::new_rgb(0x00, 0x00, 0x00));
-        cs.set_position(&center);
-        cs.set_rotation(self.angle);
-        cs.draw(target, rs);
-
-
-        for i in 1.. {
-            let xy = into_xy((i, 0), self.radius, self.angle);
-            if is_outside(&self, xy) { break }
-
-            cs.set_position(&(xy + center));
-            cs.set_fill_color(&self.hex_color((i, 0)));
-            cs.draw(target, rs);
-
-            let xy = into_xy((-i, 0), self.radius, self.angle);
-            cs.set_position(&(xy + center));
-            cs.set_fill_color(&self.hex_color((-i, 0)));
-            cs.draw(target, rs);
-
-            let xy = into_xy((0, i), self.radius, self.angle);
-            cs.set_fill_color(&self.hex_color((0, i)));
-            if is_outside(&self, xy) { continue }
-
-            cs.set_position(&(xy + center));
-            cs.draw(target, rs);
-
-            let xy = into_xy((0, -i), self.radius, self.angle);
-            cs.set_position(&(xy + center));
-            cs.set_fill_color(&self.hex_color((0, -i)));
-            cs.draw(target, rs);
+        fn rn(a: i32, b: i32) -> ::std::ops::Range<i32> {
+            if a < b { a..(b+1) }
+            else { b..(a+1) }
         }
 
-        'draw: for i in 1.. {
-            let xy = into_xy((i, 0), self.radius, self.angle);
-            if is_outside(&self, xy) { break }
+        for q in rn(c0.x, c1.x) {
+            for r in rn(c0.y, c1.y) {
+                let qr = Vector2::new(q, r);
+                let xy = into_xy(Vector2::new(qr.x as f32, qr.y as f32), size + gap, self.angle);
+                let color = self.hex_color(qr);
+                let v_it = (0..6).map(|i| {
+                    let c = hex_corner(xy, size, self.angle, i);
+                    super::Vertex {
+                        pos: [c.x / width, c.y],
+                        color: into_lrgb(color),
+                    }
+                });
 
-            for j in 1.. {
-                let xys = [
-                    ((i, j), into_xy((i, j), self.radius, self.angle)),
-                    ((-i, j), into_xy((-i, j), self.radius, self.angle)),
-                    ((i, -j), into_xy((i, -j), self.radius, self.angle)),
-                    ((-i, -j), into_xy((-i, -j), self.radius, self.angle)),
-                ];
-                if xys.iter().all(|&xy| is_outside(&self, xy.1)) { break }
-                for &xy in &xys {
-                    cs.set_position(&(xy.1 + center));
-                    cs.set_fill_color(&self.hex_color(xy.0));
-                    cs.draw(target, rs);
-                }
+                renderer.render_fan(v_it)
             }
         }
     }
